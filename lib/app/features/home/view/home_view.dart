80d78fc4f3b345ee6180acf1ca8baf24b49bdee2
noman9k <chnoman503@gmail.com>
2024-08-02 06:38:42 +0500

    sample sliver

--- lib/app/features/home/view/home_view.dart
+++ lib/app/features/home/view/home_view.dart
@@ -12,6 +12,83 @@ class HomeView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const SizedBox(child: Text(routeName));
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 180.0.h,
+            toolbarHeight: 100,
+            floating: true,
+            pinned: true,
+            primary: false,
+            snap: true,
+            backgroundColor: AppColors.primaryColor,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0),
+              ),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: SizedBox(
+                  height: 30.0,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      // isCollapsed: true,
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search, color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.white.withOpacity(0.3),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 30.0.h,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  // prefixIcon: Icon(Icons.search, color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.3),
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 100.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100.0,
+                    child: const Card(
+                      child: Text('data'),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                );
+              },
+              childCount: 100,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
